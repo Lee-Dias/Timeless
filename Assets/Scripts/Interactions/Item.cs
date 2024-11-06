742b19849e996533491ab7b8c319755bc428bd2e
lee-dias <leedias.dev@gmail.com>
2024-11-06 14:41:42 +0000

    Now items are also identified by their Era

--- Assets/Scripts/Interactions/Item.cs
+++ Assets/Scripts/Interactions/Item.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
 public class Item : ScriptableObject
 {
+    public enum Era { PreHistoric, WildWest, Medieval, Egypt};
+
     public int ID;
     public Sprite Icon;
     public string Name;
+    public Era era;
     public GameObject Prefab;
 }
