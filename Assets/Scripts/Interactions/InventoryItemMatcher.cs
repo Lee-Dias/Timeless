89f1482bea0ffd0c685d8073f334faed71a81bce
Lee-Dias <leedias.dev@gmail.com>
2024-11-07 20:32:52 +0000

    Item displayer checks if it is recieving and item from the right era and if it is the right item

--- Assets/Scripts/Interactions/InventoryItemMatcher.cs
+++ Assets/Scripts/Interactions/InventoryItemMatcher.cs
@@ -5,16 +5,32 @@ public class InventoryItemMatcher : MonoBehaviour
 {
     private PlayerInventory playerInventory;
     public UnityEvent<Item> selectedItem;
+    [SerializeField]private Item.Era era;
+    [SerializeField]private Item rightItem;
+    private bool isRightItem = false;
 
     private void Start()
     {
         playerInventory = FindFirstObjectByType<PlayerInventory>();
     }
-
     public void CheckItem()
     {
         Item selected = playerInventory.GetSelectedItem();
-        if (selected != null)   
-            selectedItem.Invoke(selected);
+        if (selected != null){     
+            if (era == selected.era){
+                if(rightItem == selected){
+                    Debug.Log("right item");
+                    isRightItem = true ;
+                }
+                selectedItem.Invoke(selected);
+            }
+            else
+            {
+                Debug.Log("wrong era");
+            }
+        }
+    }
+    public bool HasRightItem(){
+        return isRightItem;
     }
 }
