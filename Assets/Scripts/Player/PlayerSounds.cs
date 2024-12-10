097c77fc1e9c7bcae606f0c54d2042ddc3d5cd17
“[Breno <manancialgd@gmail.com>
2024-12-10 19:19:40 +0000

    Sounds are now handled correctly.

--- Assets/Scripts/Player/PlayerSounds.cs
+++ Assets/Scripts/Player/PlayerSounds.cs
@@ -7,21 +7,25 @@ public class PlayerSounds : MonoBehaviour
     [SerializeField] private Transform cameraBoober;
     private Vector3 previousCameraBobberPos;
     private bool hasPlayedFootStepSound = false;
-    
-    private void Start()
+
+    private void Awake()
     {
         GetComponent<AudioSource>().playOnAwake = false;
     }
 
     private void Update()
     {
-        if (previousCameraBobberPos.y > cameraBoober.position.y && !hasPlayedFootStepSound)
+        if (previousCameraBobberPos.y > cameraBoober.localPosition.y
+            && !hasPlayedFootStepSound)
         {
             PlayFootStepSound();
             hasPlayedFootStepSound = true;
         }
-        if (previousCameraBobberPos.y < cameraBoober.position.y) hasPlayedFootStepSound = false;
-        previousCameraBobberPos = cameraBoober.position;
+
+        if (previousCameraBobberPos.y < cameraBoober.localPosition.y)
+            hasPlayedFootStepSound = false;
+
+        previousCameraBobberPos = cameraBoober.localPosition;
     }
 
     public void PlayFootStepSound()
