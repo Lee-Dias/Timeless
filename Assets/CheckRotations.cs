26324e77cd0cfef302e0bee9176f048c67b9a84e
Lee-Dias <leedias.dev@gmail.com>
2024-12-15 19:17:01 +0000

    medieval puzzle rotate object done

---
+++ Assets/CheckRotations.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+using NaughtyAttributes;
+
+public class CheckRotations : MonoBehaviour
+{
+
+    public UnityEvent Completedpuzzle;
+
+    public void CheckChilds()
+    {
+        int totalItems = 0; 
+        int totalCorrectItems = 0; 
+        foreach (Transform child in transform)
+        {
+            totalItems += 1;
+            RotateObject rotatedObject = child.GetComponent<RotateObject>();
+
+            if(rotatedObject.hasCorrectRotation){
+                totalCorrectItems += 1;
+            }
+        }
+        if(totalCorrectItems == totalItems){
+            foreach (Transform child in transform)
+            {
+                Interactable interactable = child.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    interactable.CanInteract = false;
+                }
+            }
+        }
+        Completedpuzzle?.Invoke();
+
+    }
+}
