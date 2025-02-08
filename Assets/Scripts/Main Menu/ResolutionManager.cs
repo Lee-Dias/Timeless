b560ed20e7f7d44652b3f19be231932adab639ed
“[Breno <manancialgd@gmail.com>
2025-02-08 02:11:03 +0000

    Menus

--- Assets/Scripts/Main Menu/ResolutionManager.cs
+++ Assets/Scripts/Main Menu/ResolutionManager.cs
@@ -6,32 +6,25 @@ using UnityEngine.UI;
 
 public class ResolutionManager : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     private TextMeshProUGUI sensTextSlider;
-    [SerializeField] 
+    [SerializeField]
     private Slider senseSlider;
-    [SerializeField] 
+    [SerializeField]
     private TextMeshProUGUI volumeTextSlider;
-    [SerializeField] 
+    [SerializeField]
     private Slider volumeSlider;
-
-    private Resolution[] resolutions;
     private List<Resolution> filteredResolutions;
 
-    private float currentHZ;
-    private int currentResIndex = 0;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Update()
     {
-    }
-    void Update(){
         sensTextSlider.text = senseSlider.value.ToString("F0");
         volumeTextSlider.text = volumeSlider.value.ToString("F0");
     }
-    public void SetResolution(int resolutionIndex){
+    public void SetResolution(int resolutionIndex)
+    {
         Resolution resolution = filteredResolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height,true);
+        Screen.SetResolution(resolution.width, resolution.height, true);
 
     }
 }
